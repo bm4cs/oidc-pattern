ddf99492770732a5aed78be842c15d97d6dc8c5c
Ben Simmonds <ben@bencode.net>
2021-04-13 05:26:34 +0000

    cleaned up old containers scripts

--- todo-frontend-spa/Startup.cs
+++ todo-frontend-spa/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Buffers;
-using System.Collections.Generic;
 using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
